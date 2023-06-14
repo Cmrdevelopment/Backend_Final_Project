474b2530e93bd4593a4a9986c45a3f0439615537
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-06-14 21:54:37 +0200

    offer controller updated, pendiente revisión

--- src/api/controllers/offer.controller.js
+++ src/api/controllers/offer.controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/user.model');
 const Ratings = require('../models/ratings.model');
 const Comment = require('../models/comment.model');
+const Offer = require('../models/offer.model');
 
 //! -----------------------------------------------------------------------
 //? -------------------------------CREATE OFFER ---------------------------------
