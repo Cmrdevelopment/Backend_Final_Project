bca413a03a45a9e147fdadda3c0501c92113defe
Aitor Gutierrez <gutierrezizquierdoaitor@gmail.com>
2023-06-15 11:19:54 +0200

    rating Update

--- src/api/controllers/rating.controllers.js
+++ src/api/controllers/rating.controllers.js
@@ -19,16 +19,39 @@ const create = async (req, res, next) => {
     };
     const newRating = new Ratings(ratingBody);
     try {
+      // aqui guardamos en la base de datos
       const savedRating = await newRating.save();
       if (savedRating) {
-        await User.findByIdAndUpdate(req.body.users);
-        if (req.body.referenceOffer) {
+        // ahora lo que tenemos que guardar el id en el array de rating de quien lo creo
+        try {
+          await User.findByIdAndUpdate(req.body.users, {
+            $push: { ratingsByMe: newRating._id },
+          });
+          try {
+            if (req.body.referenceOffer) {
+              await Offer.findByIdAndUpdate(req.body.referenceOffer, {
+                $push: { ratings: newRating._id },
+              });
+              return res.status(200).json(savedRating);
+            }
+            try {
+              if (req.body.referenceUser) {
+                await User.findByIdAndUpdate(req.body.referenceUser, {
+                  $push: { ratingsByOthers: newRating._id },
+                });
+                return res.status(200).json(savedRating);
+              }
+            } catch (error) {
+              return res
+                .status(404)
+                .json("error updating user reviews with him");
+            }
+          } catch (error) {
+            return res.status(404).json("error updating referenceOffer model");
+          }
+        } catch (error) {
+          return res.status(404).json("error updating owner user rating ");
         }
-        if (req.body.referenceUser) {
-        }
-        await Ratings.findById(savedRating._id);
-
-        return res.status(200).json(savedRating);
       } else {
         return res.status(404).json("Error creating rating");
       }
@@ -36,7 +59,7 @@ const create = async (req, res, next) => {
       return res.status(404).json("error saving rating");
     }
   } catch (error) {
-    return next(error);
+    return res.status(500).json(error.message);
   }
 };
 //! -----------------------------------------------------------------------
@@ -52,27 +75,39 @@ const deleteRating = async (req, res, next) => {
     const deletedRating = await Ratings.findByIdAndDelete(id);
     if (deletedRating) {
       if (await Ratings.findById(id)) {
-        next("Fail deleting rating");
+        return res.status(404).json("failed deleting");
       } else {
-        await User.updateMany(
-          { ratings: id },
-          {
-            $pull: { ratings: id },
+        try {
+          await User.updateMany(
+            { ratings: id },
+            {
+              $pull: { ratingsByMe: id },
+              $pull: { ratingsByOthers: id },
+            }
+          );
+
+          try {
+            await Offer.updateMany(
+              { ratings: id },
+              {
+                $pull: { ratings: id },
+              }
+            );
+
+            /// por ultimo lanzamos un test en el runtime para ver si se ha borrado la review correctamente
+            return res.status(200).json({
+              deletedObject: deletedRating,
+              test: (await Ratings.findById(id))
+                ? "fail_deleting_rating"
+                : "success_deleting_rating",
+            });
+          } catch (error) {
+            return res.status(404).json("failed updating offer");
           }
-        );
-        await Offer.updateMany(
-          { ratings: id },
-          {
-            $pull: { ratings: id },
-          }
-        );
+        } catch (error) {
+          return res.status(404).json("failed updating user");
+        }
       }
-      return res.status(200).json({
-        deletedObject: deletedRating,
-        test: (await Ratings.findById(id))
-          ? "fail_deleting_rating"
-          : "success_deleting_rating",
-      });
     } else {
       return res.status(404).json({ message: "Fail deleting rating" });
     }
@@ -87,21 +122,21 @@ const deleteRating = async (req, res, next) => {
 
 const updateRating = async (req, res, next) => {
   try {
-    const { score, users, referenceUser, referenceOffer } = req.body;
-
+    const { score } = req.body;
     const filterBody = {
       score,
-      users,
-      referenceUser,
-      referenceOffer,
     };
     const { id } = req.params;
     const ratingById = await Ratings.findById(id);
     if (ratingById) {
       const patchRating = new Ratings(filterBody);
       patchRating._id = id;
-      await Ratings.findByIdAndUpdate(id, patchRating);
-      return res.status(200).json(await Ratings.findById(id));
+      try {
+        await Ratings.findByIdAndUpdate(id, patchRating);
+        return res.status(200).json(await Ratings.findById(id));
+      } catch (error) {
+        return res.status(404).json("failed updating rating");
+      }
     } else {
       return res.status(404).json({ message: "FAIL_UPDATING_RATING" });
     }
